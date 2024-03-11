3e68ccf2c470099bf0eff33e8c395754b8d64043
Aman Kumar <ak14aman2002@gmail.com>
2024-03-12 03:21:52 +0530

    Timeline updated

--- src/Components/UI/Timeline.jsx
+++ src/Components/UI/Timeline.jsx
@@ -7,6 +7,7 @@ import nitdgp from '../../Assets/Images/nitdgp.png'
 import edc from '../../Assets/Images/edc.png'
 import ieee from '../../Assets/Images/ieee.png'
 import ingenium from '../../Assets/Images/ingenium.png'
+import simplialogo from '../../Assets/Images/simplialogo.png'
 
 
 
@@ -338,6 +339,38 @@ const Timeline = () => {
                             </div>
                         </div>
                     </div>
+
+
+                    {/* RIGHT 12 Card */}
+                    <div className="mt-6 sm:mt-0 sm:mb-12">
+                        <div className="flex items-center flex-col sm:flex-row">
+                            <div className="flex justify-end w-full mx-auto items-center">
+                                <div className="w-full sm:w-1/2 sm:pl-8">
+                                    <div 
+                                    data-aos='fade-left'
+                                    data-aos-duration='800'
+                                    className=" bg-white p-4 rounded shadow-lg group hover:bg-primaryColor cursor-pointer ease-in duration-150">
+                                        <h3 className='text-primaryColor font-[700] mb-1 group-hover:text-white group-hover:font-[600] text-xl'>
+                                            Lotus Interworks
+                                        </h3>
+                                        <p className='text-[14px] text-smallTextColor group-hover:text-white group-hover:font-[600] text-l'>
+                                        Software Development Engineer 1 
+                                        <br/> July 2023 - Present
+                                        <br/> Full-Stack Developer
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                                <figure>
+                                    <img src={simplialogo} alt=""/>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+
+
 
                     </div>
                 </div>
